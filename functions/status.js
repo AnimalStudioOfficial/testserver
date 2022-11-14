1945eaf394fa0cbca1992769e80ab54821e20115
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-11-13 20:12:09 -0500

    Create status.js

---
+++ functions/status.js
@@ -0,0 +1,25 @@
+exports.handler = async (event, context) => {
+	const data = JSON.parse(event.body)
+
+	function getstatus() {
+		const xhr = new XMLHttpRequest()
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState === 4) {
+				return xhr.status === 200 ? "ok" : "error"
+			}
+		}
+		xhr.open('GET', data)
+		xhr.send()
+	}
+
+	const status = getstatus()
+	console.log(status)
+
+	if (status == "ok") {
+		document.write('OK')
+	} else {
+		document.write('Error')
+	}
+
+
+}
